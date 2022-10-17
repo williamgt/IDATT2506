c23347c3dfce4c773e1e350dbb26200c2ec70a5b
William G. Tresselt <williagt@stud.ntnu.no>
2022-10-17 15:05:14 +0200

    Changed baseline and added ToDo class

---
+++ oblig8/code/todo_list/lib/todo.dart
@@ -0,0 +1,11 @@
+class ToDo {
+  late String _description;
+  bool _done = false;
+
+  ToDo(this._description);
+
+  String get description => _description;
+  
+  bool get isDone => _done; 
+  set setDone(bool done) => _done = done; 
+}
